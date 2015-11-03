94bd1b0ba4a156bd61425d170c2c3c3f7b7cdfa0
Mieszko4 <mieszko4@gmail.com>
2015-11-03 08:46:42 +0000

    Refactor to export class

--- test/index.js
+++ test/index.js
@@ -1,69 +1,69 @@
 import assert from 'assert';
-import sortedArray from '../lib';
+import SortedArray from '../lib';
 
 describe('m-sorted-array', function () {
   it('should work with empty array', function () {
     let input = [];
     let expectedOutput = [];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with one element', function () {
     let input = [1];
     let expectedOutput = [1];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with two same elements', function () {
     let input = [7, 7];
     let expectedOutput = [7, 7];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with two ascending elements (sorted)', function () {
     let input = [1, 2];
     let expectedOutput = [1, 2];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with two descending elements (not sorted)', function () {
     let input = [2, 1];
     let expectedOutput = [1, 2];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with three elements (low, high, low)', function () {
     let input = [1, 3, 2];
     let expectedOutput = [1, 2, 3];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with three elements (high, low, high)', function () {
     let input = [6, 3, 7];
     let expectedOutput = [3, 6, 7];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with large array', function () {
     let input = [1, 7, 4, 2, 0, 9, 7];
     let expectedOutput = [0, 1, 2, 4, 7, 7, 9];
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({array: input}).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 
   it('should work with objects array and custom comparator', function () {
@@ -81,11 +81,13 @@ describe('m-sorted-array', function () {
       {id: 10}
     ];
 
-    sortedArray.comparatorFunction = function (a, b) {
-      return a.id - b.id;
-    };
-    let actualOutput = sortedArray.sort(input);
+    let actualOutput = new SortedArray({
+      array: input,
+      comparatorFunction: function (a, b) {
+        return a.id - b.id;
+      }
+    }).array;
 
-    assert.deepEqual(expectedOutput, actualOutput);
+    assert.deepEqual(actualOutput, expectedOutput);
   });
 });
